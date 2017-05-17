a07eb94cff4b7601a4843252be5161dd3c0c87d6
Matthew Jennifer <MatthewHopkins@matthews-air.coldspring.marian.edu>
2017-05-17 11:04:20 -0400

    Create a student object with an id

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 $(document).foundation()
+
+//Simple 'submit' command
+const megaroster = {
+    //Set it to an array
+    students: [],
+    
+    init(listSelector) {
+        this.studentList = document.querySelector(listSelector)
+        this.max = 0
+        document
+            .querySelector('#new-student')
+            .addEventListener('submit', this.addStudent.bind(this)) //'this' is the submission of a new name
+    },
+
+    addStudent(ev) {
+        ev.preventDefault()
+        const f = ev.target
+        const student = {
+            id: this.max + 1,
+            name: f.studentName.value,
+        }
+        this.buildListItem(student)
+        this.max ++
+    },
+
+    buildListItem(student) {
+        console.log(student)
+    },
+}
+megaroster.init('#studentList')
