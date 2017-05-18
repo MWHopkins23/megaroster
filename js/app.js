8acfe93908711f2d5c00227989d9e7ce5db3cc22
Matthew Jennifer <MatthewHopkins@matthews-air.coldspring.marian.edu>
2017-05-18 14:24:32 -0400

    Add functional down button

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
 $(document).foundation()
 
-const megaroster = {
-  students: [],
-
-  init(listSelector) {
+class Megaroster {
+  constructor(listSelector) {
     this.studentList = document.querySelector(listSelector)
+    this.students = []
     this.max = 0
     this.setupEventListeners()
     this.load()
-  },
+  }
 
   setupEventListeners() {
     document
       .querySelector('#new-student')
       .addEventListener('submit', this.addStudentViaForm.bind(this))
-  },
+  }
 
   save() {
     localStorage.setItem('roster', JSON.stringify(this.students))
-  },
+  }
 
   load() {
     const rosterString = localStorage.getItem('roster')
-    if (rosterString)
+    if (rosterString) {
       const rosterArray = JSON.parse(rosterString)
       rosterArray
         .reverse()
         .map(this.addStudent.bind(this))
-  },
+    }
+  }
 
   removeStudent(ev) {
     const btn = ev.target
@@ -43,7 +43,21 @@ const megaroster = {
 
     li.remove()
     this.save()
-  },
+  }
+
+  promoteStudent(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+    student.promoted = !student.promoted
+
+    if (student.promoted) {
+      li.classList.add('promoted')
+    } else {
+      li.classList.remove('promoted')
+    }
+    
+    this.save()
+  }
 
   addStudentViaForm(ev) {
     ev.preventDefault()
@@ -54,7 +68,7 @@ const megaroster = {
     }
     this.addStudent(student)
     f.reset()
-  },
+  }
 
   addStudent(student, append) {
     this.students.unshift(student)
@@ -66,11 +80,11 @@ const megaroster = {
       this.max = student.id
     }
     this.save()
-  },
+  }
 
   prependChild(parent, child) {
     parent.insertBefore(child, parent.firstChild)
-  },
+  }
 
   buildListItem(student) {
     const template = document.querySelector('.student.template')
@@ -79,14 +93,73 @@ const megaroster = {
     li.querySelector('.student-name').textContent = student.name
     li.dataset.id = student.id
 
+    if(student.promoted) {
+      li.classList.add('promoted')
+    }
+
+    this.setupActions(li, student)
+    return li
+  }
+
+  setupActions(li, student) {
     li
       .querySelector('button.remove')
       .addEventListener('click', this.removeStudent.bind(this))
-    return li
-  },
+
+    li
+      .querySelector('button.promote')
+      .addEventListener('click', this.promoteStudent.bind(this, student))
+
+    li
+      .querySelector('button.move-up')
+      .addEventListener('click', this.moveUp.bind(this, student))
+
+    li
+      .querySelector('button.move-down')
+      .addEventListener('click', this.moveDown.bind(this, student))
+
+  }
+
+  moveUp(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    const index = this.students.findIndex((currentStudent, i) => {
+      return currentStudent.id === student.id
+    })
+
+    if (index > 0) {
+      this.studentList.insertBefore(li, li.previousElementSibling)
+
+      const previousStudent = this.students[index - 1]
+      this.students[index - 1] = student
+      this.students[index] = previousStudent
+
+      this.save()
+    }
+  }
+
+  moveDown(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    const index = this.students.findIndex((currentStudent, i) => {
+      return currentStudent.id === student.id
+    })
+
+    if (index < this.students.length -1) {
+      this.studentList.insertBefore(li, li.nextElementSibling.nextElementSibling)
+
+      const nextStudent = this.students[index + 1]
+      this.students[index + 1] = student
+      this.students[index] = nextStudent
+
+      this.save()
+    }
+  }
 
   removeClassName(el, className){
     el.className = el.className.replace(className, '').trim()
   }
 }
-megaroster.init('#studentList')
+const roster = new Megaroster('#studentList')
