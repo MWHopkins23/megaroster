1ac223fd93aeaec3eed8df6d92862960f40e7522
Matthew Jennifer <MatthewHopkins@matthews-air.coldspring.marian.edu>
2017-05-18 09:47:06 -0400

    Persist student data in local storage

--- js/app.js
+++ js/app.js
@@ -8,24 +8,37 @@ const megaroster = {
     this.max = 0
     this.setupEventListeners()  
   },
-
+  
   setupEventListeners() {
     document
       .querySelector('#new-student')
       .addEventListener('submit', this.addStudent.bind(this))
   },
 
+  save() {
+    localStorage.setItem('roster', JSON.stringify(this.students))
+  },
+
   removeStudent(ev) {
     const btn = ev.target
-    btn.closest('.student').remove()
-    this.students.splice('.student',1)
-  },
+   const id = btn.closest('.student')
+
+    for (let i = 0; i < this.students.length; i++) {
+        if (this.students[i].id === id.dataset.id) {
+            this.students.splice(i, 1)
+            break
+            }
+        } 
+        
+        id.remove()
+        this.save()
+    },
 
   addStudent(ev) {
     ev.preventDefault()
     const f = ev.target
     const student = {
-      id: this.max + 1,
+      id: (this.max + 1).toString(),
       name: f.studentName.value,
     }
     this.students.unshift(student)
@@ -35,6 +48,7 @@ const megaroster = {
 
     this.max ++
     f.reset()
+    this.save()
   },
 
   prependChild(parent, child) {
@@ -51,7 +65,12 @@ const megaroster = {
     li
       .querySelector('button.remove')
       .addEventListener('click', this.removeStudent.bind(this))
+  //  li
+  //     .querySelector('button.up')
+  //     .addEventListener('click', this.moveUpLink.bind(this))
+   
     return li 
+
   },
 
   removeClassName(el, className){
