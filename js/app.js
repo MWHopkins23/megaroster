5f44f0a49c2609954406d67bb33014a9380673ea
Matthew Jennifer <MatthewHopkins@matthews-air.coldspring.marian.edu>
2017-05-18 10:58:11 -0400

    Make it prettier

--- js/app.js
+++ js/app.js
@@ -6,48 +6,65 @@ const megaroster = {
   init(listSelector) {
     this.studentList = document.querySelector(listSelector)
     this.max = 0
-    this.setupEventListeners()  
+    this.setupEventListeners()
+    this.load()
   },
-  
+
   setupEventListeners() {
     document
       .querySelector('#new-student')
-      .addEventListener('submit', this.addStudent.bind(this))
+      .addEventListener('submit', this.addStudentViaForm.bind(this))
   },
 
   save() {
     localStorage.setItem('roster', JSON.stringify(this.students))
   },
 
+  load() {
+    const rosterString = localStorage.getItem('roster')
+    if (rosterString)
+      const rosterArray = JSON.parse(rosterString)
+      rosterArray
+        .reverse()
+        .map(this.addStudent.bind(this))
+  },
+
   removeStudent(ev) {
     const btn = ev.target
-   const id = btn.closest('.student')
+    const li = btn.closest('.student')
 
-    for (let i = 0; i < this.students.length; i++) {
-        if (this.students[i].id === id.dataset.id) {
-            this.students.splice(i, 1)
-            break
-            }
-        } 
-        
-        id.remove()
-        this.save()
-    },
+    for (let i=0; i < this.students.length; i++) {
+      let currentId = this.students[i].id.toString()
+      if (currentId === li.dataset.id) {
+        this.students.splice(i, 1)
+        break
+      }
+    }
 
-  addStudent(ev) {
+    li.remove()
+    this.save()
+  },
+
+  addStudentViaForm(ev) {
     ev.preventDefault()
     const f = ev.target
     const student = {
-      id: (this.max + 1).toString(),
+      id: this.max + 1,
       name: f.studentName.value,
     }
+    this.addStudent(student)
+    f.reset()
+  },
+
+  addStudent(student, append) {
     this.students.unshift(student)
 
     const listItem = this.buildListItem(student)
     this.prependChild(this.studentList, listItem)
 
-    this.max ++
-    f.reset()
+    if (student.id > this.max) {
+      this.max = student.id
+    }
     this.save()
   },
 
@@ -65,12 +82,7 @@ const megaroster = {
     li
       .querySelector('button.remove')
       .addEventListener('click', this.removeStudent.bind(this))
-  //  li
-  //     .querySelector('button.up')
-  //     .addEventListener('click', this.moveUpLink.bind(this))
-   
-    return li 
-
+    return li
   },
 
   removeClassName(el, className){
